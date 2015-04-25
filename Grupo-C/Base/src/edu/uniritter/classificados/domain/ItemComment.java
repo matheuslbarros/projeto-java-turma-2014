6b066c2797a8799711c2c4697bbf96fc0f654572
Diego Porto Ritzel <diego@sistemasd.com.br>
2015-04-24 21:28:52 -0300

    Base de C e B

---
+++ Grupo-C/Base/src/edu/uniritter/classificados/domain/ItemComment.java
@@ -0,0 +1,71 @@
+package edu.uniritter.classificados.domain;
+
+import java.util.Date;
+
+public class ItemComment {
+	
+	private Long id;
+	private Item item;
+	private Date published;
+	private String title;
+	private String authorName;
+	private String authorEmail;
+	private String body;
+	private User user;
+	
+	public ItemComment() {
+	}
+	public ItemComment(Long id) {
+		this.id = id;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Item getItem() {
+		return item;
+	}
+	public void setItem(Item item) {
+		this.item = item;
+	}
+	public Date getPublished() {
+		return published;
+	}
+	public void setPublished(Date published) {
+		this.published = published;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getAuthorName() {
+		return authorName;
+	}
+	public void setAuthorName(String authorName) {
+		this.authorName = authorName;
+	}
+	public String getAuthorEmail() {
+		return authorEmail;
+	}
+	public void setAuthorEmail(String authorEmail) {
+		this.authorEmail = authorEmail;
+	}
+	public String getBody() {
+		return body;
+	}
+	public void setBody(String body) {
+		this.body = body;
+	}
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+
+}
---
+++ Grupo-C/Base/src/edu/uniritter/classificados/dto/CategoryDTO.java
@@ -0,0 +1,36 @@
+package edu.uniritter.classificados.dto;
+
+public class CategoryDTO {
+	
+	private Long id;
+	private String name;
+	private Integer position;
+	
+	public CategoryDTO() {
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getPosition() {
+		return position;
+	}
+
+	public void setPosition(Integer position) {
+		this.position = position;
+	}
+	
+}
